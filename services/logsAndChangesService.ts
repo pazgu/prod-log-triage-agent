e007a25c56720d4aebb73ca44345a484641c6e74
pazgu <pazgueta@gmail.com>
2026-07-23 18:44:57 +0300

    Refactor formatting and indentation in logsAndChangesService.ts for improved readability

--- services/logsAndChangesService.ts
+++ services/logsAndChangesService.ts
@@ -1,19 +1,22 @@
-import { LogEntry, RecentChanges } from '../agent/types.js';
+import { LogEntry, RecentChanges } from "../agent/types.js";
 
-const getLogsAndChanges = async (logFileNumber: number): Promise<{ logs: LogEntry[], changes: RecentChanges[] }> => {
-    const getLogFileName = () => `log_set_${logFileNumber}.ts`;
-    const logFile = (await import(`../prod_logs/${getLogFileName()}`));
+const getLogsAndChanges = async (
+  logFileNumber: number,
+): Promise<{ logs: LogEntry[]; changes: RecentChanges[] }> => {
+  const getLogFileName = () => `log_set_${logFileNumber}.ts`;
+  const logFile = await import(`../prod_logs/${getLogFileName()}`);
 
-    return { logs: logFile.LOGS, changes: logFile.RECENT_CHANGES };
+  return { logs: logFile.LOGS, changes: logFile.RECENT_CHANGES };
 };
 
 export const loadLogs = async (logFileNumber: number): Promise<LogEntry[]> => {
-    const logs = await getLogsAndChanges(logFileNumber);
-    return logs.logs;
+  const logs = await getLogsAndChanges(logFileNumber);
+  return logs.logs;
 };
 
-export const loadRecentChanges = async (logFileNumber: number): Promise<RecentChanges[]> => {
-    const logs = await getLogsAndChanges(logFileNumber);
-    return logs.changes;
+export const loadRecentChanges = async (
+  logFileNumber: number,
+): Promise<RecentChanges[]> => {
+  const logs = await getLogsAndChanges(logFileNumber);
+  return logs.changes;
 };
-
